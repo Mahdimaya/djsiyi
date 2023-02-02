83600fe37afcc76acbfa7e7d4f3917af22cc4048
Maya Mahdi <115892209+Mahdimaya@users.noreply.github.com>
2023-02-02 12:52:38 +0100

    Update auth.py
    Correction

--- snowy/website/auth.py
+++ snowy/website/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint ,render_template, request
+from flask import Blueprint ,render_template, request, flash 
 
 auth = Blueprint('auth', __name__)
 
@@ -14,22 +14,22 @@ def logout():
 
 @auth.route('/sign-up')
 def sign_up():
-    if request.method == 'POST'
-    email= request.form.get('email')
-    firstname= request.form.get('firstname')
-    lastname= request.form.get('lastname')
-    password= request.form.get('password')
+    if request.method == 'POST':
+      email= request.form.get('email')
+      firstname= request.form.get('firstname')
+      lastname= request.form.get('lastname')
+      password= request.form.get('password')
     username= request.form.get('username')
 
     if len (email) < 4:
         flash('email must be greater than 4 characters', category='error')
-       pass
+        pass
     elif len (firstname) < 2:
         flash('firstname must be greater than 2 characters', category='error')
-       pass
+        pass
     elif len (lastname) < 4:
         flash('lastname must be greater than 2 characters', category='error')
-       pass
+        pass
     elif len (password) <7:
         flash('password must be greater than 7 characters', category='error')
         pass
